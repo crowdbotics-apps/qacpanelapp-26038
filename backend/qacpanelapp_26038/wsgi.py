d6edb75305bcdc940626527cbdffd690cd79d8dd
Crowdbotics <team@crowdbotics.com>
2021-04-30 07:11:20 +0000

    Update models

--- backend/qacpanelapp_26038/wsgi.py
+++ backend/qacpanelapp_26038/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qacpanelapp_26038.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qacpanelapp_26038.settings")
 
 application = get_wsgi_application()
